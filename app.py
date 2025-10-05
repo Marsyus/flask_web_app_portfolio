a01b184590e040f4b173ae59b5b3e6e2474d2e92
Marsyus <miadolomboy0917@gmail.com>
2025-10-05 15:02:53 +0800

    Added index file as the home page

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# HOME
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
