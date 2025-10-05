e02dbabf884687e5b9d1031bc485ff9b38370a5a
Marsyus <miadolomboy0917@gmail.com>
2025-10-05 15:06:46 +0800

    Added path to profile with the use of JavaScript

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# PROFILE
+@app.route('/profile.html')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
