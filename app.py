e611f97f79580d66405792c4d612fe3433ea636e
Marsyus <miadolomboy0917@gmail.com>
2025-10-05 15:23:55 +0800

    Added spacing for readability

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, render_template, request 
 from math import pi
 
+
 app = Flask(__name__)
+
 
 # HOME
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 # PROFILE
 @app.route('/profile.html')
 def profile():
     return render_template('profile.html')
 
+
 # PROGRAMMING WORKS
 @app.route('/works.html')
 def works():
     return render_template('works.html')
+
 
 # TO UPPERCASE
 @app.route('/to-uppercase.html', methods=['GET', 'POST'])
@@ -30,6 +35,7 @@ def to_uppercase():
             result = "Invalid input. Please enter a string."
     return render_template('to-uppercase.html', result=result)
 
+
 # AREA OF CIRCLE
 @app.route('/area-of-circle.html', methods=['GET', 'POST'])
 def area_of_circle():
@@ -41,6 +47,7 @@ def area_of_circle():
         except ValueError:
             error = "Invalid input. Please enter a number."
     return render_template('area-of-circle.html', result=result, error=error)
+
 
 # AREA OF TRIANGLE
 @app.route('/area-of-triangle.html', methods=['GET', 'POST'])
@@ -54,6 +61,7 @@ def area_of_triangle():
         except ValueError:
             error = "Invalid input. Please enter a number."
     return render_template('area-of-triangle.html', result=result, error=error)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
