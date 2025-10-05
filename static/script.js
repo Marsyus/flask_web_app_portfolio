800d56106244f4acfc700b0a0f37b515c73ddc74
Marsyus <miadolomboy0917@gmail.com>
2025-10-05 15:15:36 +0800

    Added navigation to files within programming works

--- static/script.js
+++ static/script.js
@@ -5,3 +5,15 @@ function goToProfile() {
 function goToWorks() {
     window.location.href = "/works.html"
 }
+
+function goToUppercase() {
+    window.location.href = "/to-uppercase.html";
+}
+
+function goToAreaOfCircle() {
+    window.location.href = "/area-of-circle.html";
+}
+
+function goToAreaOfTriangle() {
+    window.location.href = "/area-of-triangle.html";
+}
