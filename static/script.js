4baa8a4210998987e42336dd0ea5fc786688cc07
Marsyus <miadolomboy0917@gmail.com>
2025-10-08 13:17:34 +0800

    Designed the contact page with links

--- static/script.js
+++ static/script.js
@@ -1,27 +1,64 @@
+// Home navigation
 function goToHome() {
     window.location.href = "/";
 }
 
+
+// Profile navigation
 function goToProfile() {
     window.location.href = "/profile.html";
 }
 
+
+// Contact navigation
 function goToContact() {
     window.location.href = "/contact.html";
 }
 
+
+// Facebook navigation
+function goToFacebook() {
+    window.open("https://www.facebook.com/marxius.py", "_blank");
+}
+
+
+// Gmail navigation
+function goToGmail() {
+    window.open("mailto:miadolomboy0917@gmail.com", "_blank");
+}
+
+
+// GitHub navigation
+function goToGitHub() {
+    window.open("https://github.com/Marsyus", "_blank");
+}
+
+
+// LinkedIn navigation
+function goToLinkedIn() {
+    window.open("https://www.linkedin.com/in/marxius-ivan-adolf-denniel-lomboy-05680b354/", "_blank");
+}
+
+
+// Works navigation
 function goToWorks() {
     window.location.href = "/works.html"
 }
 
+
+// Convert to Uppercase navigation
 function goToUppercase() {
     window.location.href = "/to-uppercase.html";
 }
 
+
+// Area of Circle navigation
 function goToAreaOfCircle() {
     window.location.href = "/area-of-circle.html";
 }
 
+
+// Area of Triangle navigation
 function goToAreaOfTriangle() {
     window.location.href = "/area-of-triangle.html";
 }
